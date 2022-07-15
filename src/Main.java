9878817e08121e579b4308ca4cb9d6403ddfad2d
Berdigulov <berdigulovvv@gmail.com>
2022-07-15 11:18:04 +0300

    доп

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
         Boss boss1 = new Boss();
         boss1.setHealth(500);
@@ -7,5 +10,20 @@ public class Main {
         System.out.println("Boss's health: " + boss1.getHealth());
         System.out.println("Boss's damage: " + boss1.getDamage());
         System.out.println("Type of defense of Boss: " + boss1.getTypeOfDefense());
+
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println(createHeroes()[i].getHealth() + createHeroes()[i].getDamage() +
+                    createHeroes()[i].getSuperPower());
+
+        }
+
+    }
+
+    public static Hero[] createHeroes(){
+        Hero wizard = new Hero (150,50);
+        Hero warrior = new Hero (200, 45,"Rampage");
+        Hero doctor = new Hero (160 ,0,"Healing");
+        Hero heroes [] = new Hero[] {warrior,wizard,doctor};
+        return heroes;
     }
 }
